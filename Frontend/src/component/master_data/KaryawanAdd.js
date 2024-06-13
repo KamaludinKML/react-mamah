dbb887d0b04232dd6a7a3cfb0de6d482f5914350
KamaludinKML <kamalnasrullah12az@gmail.com>
2024-06-13 11:35:04 +0700

    karyawan

--- Frontend/src/component/master_data/KaryawanAdd.js
+++ Frontend/src/component/master_data/KaryawanAdd.js
@@ -4,7 +4,6 @@ import React from "react";
 
 function KaryawanAdd() {
   const [formValue, setformValue] = React.useState({
-    id:"",
     nama_karyawan: "",
     tgl_lahir: "",
     jenis_kelamin: "",
@@ -17,33 +16,32 @@ function KaryawanAdd() {
       ...formValue,
       [event.target.name]: event.target.value,
     });
-  }
+  };
 
   const handleSubmit = async () => {
     // store the states in the form data
     const FormDataInput = new FormData();
-    FormDataInput.append("id", formValue.id);
     FormDataInput.append("nama_karyawan", formValue.nama_karyawan);
     FormDataInput.append("tgl_lahir", formValue.tgl_lahir);
     FormDataInput.append("jenis_kelamin", formValue.jenis_kelamin);
     FormDataInput.append("alamat", formValue.alamat);
     FormDataInput.append("noTlp", formValue.noTlp);
-
     alert("Data berhasil disimpan");
     try {
       // make axios post request
       const response = await axios({
-        method: "POST",
+        method: "post",
         url: "https://localhost:7092/Karyawan/CreateKaryawan",
         data: FormDataInput,
         headers: { "Content-Type": "application/json" },
       });
+      window.location.href = "/datakaryawan";
       console.log(response);
     } catch (error) {
       console.log(error);
       alert(error);
     }
-  }
+  };
 
   return (
     <div className="card">
@@ -51,13 +49,6 @@ function KaryawanAdd() {
         <div className="Titel">Tambah Data Karyawan</div>
         <div className="conten">
           <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="id"
-              placeholder="enter Id"
-              value={formValue.id}
-              onChange={handleChange}
-            />
             <br />
             <br />
             <input
@@ -105,7 +96,9 @@ function KaryawanAdd() {
             />
             <br />
             <br />
-            <button type="submit" className="btn btn-primary">Simpan</button>
+            <button type="submit" className="btn btn-primary">
+              Simpan
+            </button>
           </form>
         </div>
       </div>
@@ -113,4 +106,4 @@ function KaryawanAdd() {
   );
 }
 
-    export default KaryawanAdd;
+export default KaryawanAdd;
---
+++ Frontend/src/component/master_data/KaryawanDelete.js
@@ -0,0 +1,90 @@
+import "./Mahasiswa.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+function KaryawanDelete() {
+  const { id } = useParams();
+  //define state
+  // Define state
+  const [karyawan, setKaryawan] = useState({
+    id: "",
+    nama: "",
+    email: "",
+    password: "",
+    nohp: "",
+    status: "",
+  });
+
+  //useEffect hook
+  React.useEffect(() => {
+    //panggil method "fetchData"
+    fectData();
+  }, []);
+
+  //function "fetchData"
+  const fectData = async () => {
+    try {
+      //fetching
+      const response = await axios.get(
+        `https://localhost:7092/Karyawan/GetByIdKaryawan?id=${id}`
+      );
+      //get response data
+      // Get response data
+      const data = await response.data;
+      const dataid = data[0].id;
+      const datanama = data[0].nama;
+      const dataemail = data[0].email;
+      const datapassword = data[0].password;
+      const datanohp = data[0].nohp;
+
+      // Assign response data to state "karyawan"
+      setKaryawan({
+        id: dataid,
+        nama: datanama,
+        email: dataemail,
+        password: datapassword,
+        nohp: datanohp,
+      });
+    } catch (error) {
+      console.log(error);
+      alert("Data tidak ditemukan atau sudah dihapus!");
+    }
+  };
+
+  // Handle form submission
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      // make axios post request
+      await axios.delete(
+        `https://localhost:7092/Karyawan/DeleteKaryawan?id=${id}`
+      );
+      // Redirect back to the master data after successful deletion
+      window.location.href = "/datakaryawan";
+
+      alert("Data berhasil dihapus");
+    } catch (error) {
+      console.error(error);
+      alert("Error deleting data");
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className="container">
+        <div className="Titel">Hapus Data Karyawan {id}</div>
+        <div className="conten">
+          <form onSubmit={handleSubmit}>
+            <p>yakin</p>
+            <button type="submit" className="btn btn-danger">
+              Hapus
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default KaryawanDelete;
